283d9165a2e58b16a48a163857e943e745193534
LZKDreamer <1254233340@qq.com>
2019-08-06 15:45:45 +0800

    Initial commit

---
+++ app/src/main/java/com/lzk/newsbox/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lzk.newsbox.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lzk.newsbox.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
